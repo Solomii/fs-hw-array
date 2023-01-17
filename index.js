a3684b1e6132393ac4348c7426c4f9ebdc86f430
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-01-17 12:42:05 +0200

    Add work task

--- index.js
+++ index.js
@@ -2,8 +2,25 @@
 Написати функцію, яка приймає масив чисел
 і повертає true, якщо в цьому масиві є два однакових числа поспіль
 Якщо не має - повертає false, якщо масив порожній - теж повертає false
-
-
 // [1,2,3,4,5]->false
 // [1,2,2,3,2]->true
  */
+
+function checkTwoIdenticalNumbers(arr) {
+    if (arr.length === 0 || arr.length ===1) {
+        return "false";
+    }
+    // debugger
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i-1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(checkTwoIdenticalNumbers([]));
+console.log(checkTwoIdenticalNumbers([1, 2, 2, 3, 2]));
+console.log(checkTwoIdenticalNumbers([3,3]));
+console.log(checkTwoIdenticalNumbers([1, 2, 3, 4, 5]));
+console.log(checkTwoIdenticalNumbers([1]));
